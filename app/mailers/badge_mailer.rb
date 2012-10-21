8344d06afd14174bc336d79d1d958549c7e1983b
David Troy <dave@410labs.com>
2012-10-21 11:35:09 -0400

    remove priority header for now

--- app/mailers/badge_mailer.rb
+++ app/mailers/badge_mailer.rb
@@ -4,7 +4,7 @@ class BadgeMailer < ActionMailer::Base
   def please_edit(badge)
     @badge = badge
     mail(:to => @badge.email,
-         :subject => "[TEDxMidAtlantic] Important: Please create a badge for #{badge.ticketholder}",
-         :headers => { 'X-Priority' => '1' })
+         :subject => "[TEDxMidAtlantic] Important: Please create a badge for #{badge.ticketholder}")
+         #'X-Priority' => '1')
   end
 end
